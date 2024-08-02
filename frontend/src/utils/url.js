428c23d4f9535a5bd746e865e7f8efaef3bc14d5
darshah0205 <160843795+darshah0205@users.noreply.github.com>
2024-08-02 20:21:21 +0530

    Update url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:8000/api";
-export const pdfURL = "http://localhost:8000/uploads";
+export const url = "http://localhost:5000/api";
+export const pdfURL = "http://localhost:5000/uploads";
